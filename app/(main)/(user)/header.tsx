2c22bcfee1f357f29de5b2f33597741e65358df8
Sammed S P <127593704+psammy171@users.noreply.github.com>
2024-04-13 22:22:29 +0530

    feat updated header
    modified header based on authentication status

--- app/(main)/(user)/header.tsx
+++ app/(main)/(user)/header.tsx
@@ -1,9 +1,14 @@
+"use client";
+
 import CartIcon from "@/components/icons/cart";
 import OrdersIcon from "@/components/icons/orders";
 import StoreIcon from "@/components/icons/store";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
 
-const Header = async () => {
+const Header = () => {
+  const { data: session, status } = useSession();
+
   return (
     <div className="h-12 bg-primary-900">
       <div className="container mx-auto h-full flex px-1 items-center">
@@ -12,17 +17,37 @@ const Header = async () => {
         </Link>
         <p className="mx-2 text-white italic font-semibold">Grocery Store</p>
         <span className="flex-grow"></span>
-        <Link href={"/my-orders"}>
-          <p className="text-white cursor-pointer hover:scale-105 transition-all">
-            Order History
-          </p>
-        </Link>
-        <Link href={"/cart"} className="mx-4">
-          <CartIcon className="text-white w-5 h-5 cursor-pointer hover:scale-110 transition-all" />
-        </Link>
-        <span className="w-7 h-7 rounded-full bg-gray-200 flex items-center justify-center cursor-pointer text-primary-900">
-          G
-        </span>
+        {status === "authenticated" ? (
+          <>
+            <Link href={"/my-orders"}>
+              <p className="text-white cursor-pointer hover:scale-105 transition-all">
+                Order History
+              </p>
+            </Link>
+            <Link href={"/cart"} className="mx-4">
+              <CartIcon className="text-white w-5 h-5 cursor-pointer hover:scale-110 transition-all" />
+            </Link>
+            <span className="w-7 h-7 rounded-full bg-gray-200 flex items-center justify-center cursor-pointer text-primary-900">
+              G
+            </span>
+          </>
+        ) : (
+          <>
+            <Link
+              href={"/auth/login"}
+              className="text-white hover:scale-105 transition-all"
+            >
+              Login
+            </Link>
+            <div className="w-[1px] h-6 bg-gray-200 mx-2"></div>
+            <Link
+              href={"/auth/signup"}
+              className="text-white hover:scale-105 transition-all"
+            >
+              Sign up
+            </Link>
+          </>
+        )}
       </div>
     </div>
   );
