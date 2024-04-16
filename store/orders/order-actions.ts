8cba38b02fd30558984a64c7f5da026d9ce4994f
Sammed S P <127593704+psammy171@users.noreply.github.com>
2024-04-16 10:17:13 +0530

    feat all orders
    added user specific type

--- store/orders/order-actions.ts
+++ store/orders/order-actions.ts
@@ -1,6 +1,6 @@
 import { Axios } from "axios";
 import toast from "react-hot-toast";
-import { orderActions } from "./order-slice";
+import { OrderByUser, orderActions } from "./order-slice";
 
 export const getMyOrders = (axios: Axios) => {
   return async (dispatch: any) => {
@@ -17,7 +17,34 @@ export const getAllOrders = (axios: Axios) => {
   return async (dispatch: any) => {
     try {
       const res = await axios.get("/orders");
-      dispatch(orderActions.init({ orders: res.data }));
+      const orders = res.data;
+      const list: any = {};
+      for (const order of orders) {
+        list[order.user?.email] = list[order.user.email]
+          ? [...list[order.user.email], order]
+          : [order];
+      }
+      const ordersByUser: OrderByUser[] = [];
+      for (const key in list) {
+        const newObj = {
+          user: list[key][0].user,
+          orders: list[key].map((item: any) => {
+            delete item.user;
+            return item;
+          }),
+        };
+        ordersByUser.push(newObj);
+      }
+      ordersByUser.sort((a, b) => {
+        if (a.user.email > b.user.email) return 1;
+        if (a.user.email < b.user.email) return -1;
+        return 0;
+      });
+      dispatch(
+        orderActions.initOrdersByUser({
+          ordersByUser: ordersByUser,
+        })
+      );
     } catch (err) {
       toast.error("Something went wrong!");
     }
--- store/orders/order-slice.ts
+++ store/orders/order-slice.ts
@@ -1,5 +1,6 @@
 import { Address } from "@/types/address";
 import { CartItem } from "@/types/cart-item";
+import { User } from "@/types/user";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export type Order = {
@@ -9,14 +10,21 @@ export type Order = {
   address: Address | null;
 };
 
+export type OrderByUser = {
+  user: User;
+  orders: [];
+};
+
 type SliceType = {
   loading: boolean;
   ordres: Order[];
+  ordersByUser: OrderByUser[];
 };
 
 const initialState: SliceType = {
   loading: true,
   ordres: [],
+  ordersByUser: [],
 };
 
 const orderSlice = createSlice({
@@ -25,6 +33,13 @@ const orderSlice = createSlice({
   reducers: {
     init: (state, action: PayloadAction<{ orders: Order[] }>) => {
       state.ordres = action.payload.orders;
+      state.loading = false;
+    },
+    initOrdersByUser: (
+      state,
+      action: PayloadAction<{ ordersByUser: OrderByUser[] }>
+    ) => {
+      state.ordersByUser = action.payload.ordersByUser;
       state.loading = false;
     },
   },
