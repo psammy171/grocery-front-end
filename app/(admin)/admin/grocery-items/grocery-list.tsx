87e3406a9bd4575fb46fb32ee658b847e59ebade
Sammed S P <127593704+psammy171@users.noreply.github.com>
2024-04-17 00:22:16 +0530

    feat search bar
    added search bar to home page and grocery list

--- app/(admin)/admin/grocery-items/grocery-list.tsx
+++ app/(admin)/admin/grocery-items/grocery-list.tsx
@@ -3,12 +3,14 @@
 import { useAppDispatch, useAppSelector } from "@/store";
 import GroceryItem from "./grocery-item";
 import useAxios from "@/lib/api/use-axios";
-import { useEffect } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { initializeItems } from "@/store/items/item-actions";
+import SearchIcon from "@/components/icons/search";
 
 const GroceryList = () => {
   const axios = useAxios();
   const dispatch = useAppDispatch();
+  const [search, setSearch] = useState<string>("");
   const loading = useAppSelector((state) => state.items.loading);
   const items = useAppSelector((state) => state.items.items);
 
@@ -21,10 +23,27 @@ const GroceryList = () => {
       {loading && <p>Loading...</p>}
       {!loading && items.length === 0 && <p>No items created yet</p>}
       {!loading && items.length > 0 && (
-        <div className="flex flex-wrap gap-4 mb-4">
-          {items.map((item) => (
-            <GroceryItem key={item.id} item={item} axios={axios} />
-          ))}
+        <div>
+          <div className="relative mb-4">
+            <input
+              className="mx-auto peer bg-gray-100 border-none transition-all rounded focus:w-72  focus:ring-2 focus:ring-primary-900 py-2 w-40 pl-8"
+              placeholder="Search"
+              onChange={(e: any) => setSearch(e.target.value)}
+            />
+            <SearchIcon className="w-5 h-5 absolute mx-[6px] top-[10px] text-gray-500 peer-focus:text-gray-800 transition-colors duration-300" />
+          </div>
+          <div className="flex flex-wrap gap-4 mb-4">
+            {items
+              .filter((item) =>
+                item.name
+                  .replaceAll(" ", "")
+                  .toLowerCase()
+                  .includes(search.replaceAll(" ", "").toLowerCase())
+              )
+              .map((item) => (
+                <GroceryItem key={item.id} item={item} axios={axios} />
+              ))}
+          </div>
         </div>
       )}
     </div>
--- app/(main)/(user)/item-list.tsx
+++ app/(main)/(user)/item-list.tsx
@@ -2,6 +2,8 @@ import { useAppDispatch, useAppSelector } from "@/store";
 import { Item } from "@/types/Item";
 import { addItemToCart, removeItemFromCart } from "@/store/cart/cart-actions";
 import useAxios from "@/lib/api/use-axios";
+import SearchIcon from "@/components/icons/search";
+import { useState } from "react";
 
 const ItemComponent = ({
   item,
@@ -51,6 +53,7 @@ const ItemComponent = ({
 };
 
 const ItemList = () => {
+  const [search, setSearch] = useState<string>("");
   const items = useAppSelector((state) => state.items.items);
   const loading = useAppSelector((state) => state.items.loading);
   const cartItems = useAppSelector((state) => state.cart.items);
@@ -67,14 +70,33 @@ const ItemList = () => {
     <div>
       {loading && <p>Fetching items</p>}
       {!loading && (
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {items.map((item) => (
-            <ItemComponent
-              key={item.id}
-              item={item}
-              quantity={getCartCount(item)}
-            />
-          ))}
+        <div>
+          <div className="flex justify-center mb-4">
+            <div className="mx-auto relative">
+              <input
+                className="mx-auto peer bg-gray-100 border-none transition-all rounded focus:w-72  focus:ring-2 focus:ring-primary-900 py-2 w-40 pl-8"
+                placeholder="Search"
+                onChange={(e: any) => setSearch(e.target.value)}
+              />
+              <SearchIcon className="w-5 h-5 absolute mx-[6px] top-[10px] text-gray-500 peer-focus:text-gray-800 transition-colors duration-300" />
+            </div>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {items
+              .filter((item) =>
+                item.name
+                  .replaceAll(" ", "")
+                  .toLowerCase()
+                  .includes(search.replaceAll(" ", "").toLowerCase())
+              )
+              .map((item) => (
+                <ItemComponent
+                  key={item.id}
+                  item={item}
+                  quantity={getCartCount(item)}
+                />
+              ))}
+          </div>
         </div>
       )}
     </div>
